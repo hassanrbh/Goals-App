5785573295364b59931f7764fc54a67ed49e9c27
hassantarif <hassantarif4@gmail.com>
2022-03-30 20:06:01 +0000

    adding specs

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+<<<<<<< HEAD
   # root "articles#index"
+=======
+  root "users#index"
+  resources :users
+>>>>>>> parent of 9f21dd8 (adding integretion and end to end sweet tests)
 end
