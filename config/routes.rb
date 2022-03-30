ecb59278f565113d6d3ad2e47b7d71326bdd8b27
hassantarif <hassantarif4@gmail.com>
2022-03-30 12:41:35 +0000

    Revert "adding sweet tests for users and users controller"
    This reverts commit e532a654843628e390da3027e5777a1ca2a63b6e.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
-  resources :users
-  resources :sessions
+  # root "articles#index"
 end
