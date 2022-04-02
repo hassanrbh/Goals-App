d8171c08ff833823b30c269ee9b0ff2688fdd886
hassantarif <hassantarif4@gmail.com>
2022-04-02 19:48:25 +0000

    adding comments to the goals table

--- app/controllers/goal_comments_controller.rb
+++ app/controllers/goal_comments_controller.rb
@@ -1,2 +1,17 @@
 class GoalCommentsController < ApplicationController
+    def create
+        goal_comment = GoalComment.new(goal_comments_params)
+        if goal_comment.save
+            redirect_to intention_path(params[:goal_comments][:intention_id])
+            flash[:success] = "Comment created successfully"
+        else
+            redirect_to intention_path(params[:goal_comments][:intention_id])
+            flash[:error] = goal_comment.errors.full_messages
+        end
+    end
+
+    private
+    def goal_comments_params
+        params.require(:goal_comments).permit(:comment, :intention_id)
+    end
 end
